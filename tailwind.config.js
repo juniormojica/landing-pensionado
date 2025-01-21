32ac295f0c6f18fc71a6074c9ec9902689e3b644
junior mojica <juniormojica26@gmail.com>
2025-01-21 08:55:32 -0500

    adding default color to taildwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ export default {
   content: [ './index.html',
     './src/**/*.{js,ts,jsx,tsx}',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlue: "#0D47A1", // Azul Marino
+        secondaryYellow: "#FFC107", // Amarillo Mostaza
+        accentGreen: "#4CAF50", // Verde Esmeralda
+        lightGray: "#F5F5F5", // Gris Claro
+        darkGray: "#757575", // Gris Oscuro
+        white: "#FFFFFF", // Blanco
+      },
+    },
   },
   plugins: [],
 }
