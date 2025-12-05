4fb777de299211c1b3ecb1505e9203c6241a59a4
jr mojica <juniormojica26@gmail.com>
2025-12-05 08:20:00 -0500

    street view map changed to normal map view

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,18 +1,18 @@
 const Map = () => {
-    return (
-      <div className="w-full max-w-4xl mx-auto p-4">
-        <div className="relative w-full aspect-video rounded-lg overflow-hidden shadow-lg">
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!4v1737390562768!6m8!1m7!1sMB7GKTIc64c1ZZmKqRCFxQ!2m2!1d10.4523593110531!2d-73.26415768060156!3f29.198432498013545!4f5.655902481382483!5f0.7820865974627469" 
-            className="absolute top-0 left-0 w-full h-full"
-            style={{border: 0}}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          />
-        </div>
+  return (
+    <div className="w-full max-w-4xl mx-auto p-4">
+      <div className="relative w-full aspect-video rounded-lg overflow-hidden shadow-lg">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.8!2d-73.26415768060156!3d10.4523593110531!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTDCsDI3JzA4LjUiTiA3M8KwMTUnNTEuMCJX!5e0!3m2!1ses!2sco!4v1737390562768!5m2!1ses!2sco"
+          className="absolute top-0 left-0 w-full h-full"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
       </div>
-    );
-  };
-  
-  export default Map;
+    </div>
+  );
+};
+
+export default Map;
